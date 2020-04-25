86d999b0d2f75ca0cb3d893747999352e8bbdb7c
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-25 03:35:40 +0000

    Automatically backed up by Learn

--- mytest.rb
+++ mytest.rb
@@ -1,12 +1,12 @@
 
 array=[1,2,3]
-value_to_find=3
+#value_to_find=3
 
   counter=0
   while counter < array.size do
-    if array[counter]== value_to_find
+#    if array[counter]== value_to_find
       puts counter
-    else
+#    else
       counter += 1
   end
   
