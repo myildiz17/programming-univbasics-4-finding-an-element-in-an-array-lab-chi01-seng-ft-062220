f47a459b6d7ebecd35097a7e80bbf688df916d5d
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-25 04:02:22 +0000

    Done.

---
+++ mytest.rb
@@ -0,0 +1,14 @@
+
+array=[1,2,3]
+
+def find_in_array (array,value_to_find)
+  counter=0
+  while counter < array.size do
+    if array[counter]== value_to_find
+   return counter
+    end
+      counter += 1
+  end
+end
+
+find_in_array(array,3)
