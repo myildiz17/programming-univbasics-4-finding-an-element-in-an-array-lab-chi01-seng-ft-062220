7751c2d009171e2ece7c10eb79c7e7b4a7e16ff5
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-25 03:40:42 +0000

    Automatically backed up by Learn

--- mytest.rb
+++ mytest.rb
@@ -6,13 +6,13 @@ value_to_find=3
   while counter < array.size do
 if array[counter]== value_to_find
    puts "yasasin"
-  
+
 else
   puts "not lucky"
-  
+
 end
 
-   
+
 
 #    else
       counter += 1
