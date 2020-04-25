18d85f2085843b50f2ab1c8e5c9fa6487a2f348c
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-25 03:56:17 +0000

    Automatically backed up by Learn

--- mytest.rb
+++ mytest.rb
@@ -1,7 +1,7 @@
 
 array=[1,2,3]
 
-def find_in_array (array,value_to_find)
+#def find_in_array (array,value_to_find)
   counter=0
   while counter < array.size do
     if array[counter]== value_to_find
@@ -9,6 +9,6 @@ def find_in_array (array,value_to_find)
     end
       counter += 1
   end
-end
+#end
 
-find_in_array(array,3)
+#find_in_array(array,3)
