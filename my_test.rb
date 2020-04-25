ad5522bb4cdcd0b64ef745ea4bfdcd6c19dc44e3
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-25 03:34:39 +0000

    Automatically backed up by Learn

---
+++ my_test.rb
@@ -0,0 +1,11 @@
+array=[1,2,3,4]
+
+def find_element_index(array, value_to_find)
+  count = 0
+  while count < array.length do
+    puts array[count] == value_to_find
+    count += 1
+  end
+end
+
+find_element_index(array, 3)
