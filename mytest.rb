ab7207cea5816a8148fd097482c894fb595ade36
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-04-25 03:57:37 +0000

    Automatically backed up by Learn

--- mytest.rb
+++ mytest.rb
@@ -5,7 +5,7 @@ def find_in_array (array,value_to_find)
   counter=0
   while counter < array.size do
     if array[counter]== value_to_find
-   puts counter
+   return counter
     end
       counter += 1
   end
